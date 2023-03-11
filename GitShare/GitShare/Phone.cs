912302f2d1d2f3d8e4862a0b9c55660ba9eb27dc
KardasBartosz <94651023+KardasBartosz@users.noreply.github.com>
2023-03-11 15:09:21 +0100

    add new class

--- GitShare/GitShare/Phone.cs
+++ GitShare/GitShare/Phone.cs
@@ -11,5 +11,7 @@ namespace GitShare
         public string OwnerName { get; set; }
         public string OwnerSurname { get; set; }
         public string PhoneNumber { get; set; }
+
+        public string PhoneModel { get; set; }
     }
 }
