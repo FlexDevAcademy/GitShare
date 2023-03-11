89c80607ee29ec29ac4f6ec0e4c6899b92c3e894
KardasBartosz <94651023+KardasBartosz@users.noreply.github.com>
2023-03-11 15:27:30 +0100

    changing string to int

--- GitShare/GitShare/Note.cs
+++ GitShare/GitShare/Note.cs
@@ -8,7 +8,7 @@ namespace GitShare
 {
     internal class Note
     {
-        public string HeaderName { get; set; }
+        public int HeaderName { get; set; }
         public string Title { get; set; }
     }
 }
