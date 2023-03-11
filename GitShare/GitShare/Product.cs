b5262cdabb40c83897f9403e24579112a4a31996
daria956 <125731362+daria956@users.noreply.github.com>
2023-03-11 15:43:19 +0100

    change int Price na double Price

--- GitShare/GitShare/Product.cs
+++ GitShare/GitShare/Product.cs
@@ -9,7 +9,7 @@ namespace GitShare
     public class Product
     {
         private string Name { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
         public string Description { get; set; }
         public int Size { get; set; }
     }
